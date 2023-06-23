db79745cd3e8f457beac0999b9d6c9a4b5fda1fd
Cynthyana <96613566+Cynthyana@users.noreply.github.com>
2023-06-23 19:19:33 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,29 @@ function preload(){}
 function setup(){
     canvas = createCanvas(500, 500);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500,500);
+    video.hide();
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw(){}
+function modelLoaded(){
+    console.log('posenet initialised :)')
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log('nose x = ' + results[0].pose.nose.x);
+        console.log('nose y = ' + results[0].nose.pose.y)
+    }
+}
+
+function draw(){
+    image(video,0,0,500,500);
+}
 
 function take_snapshot(){
-    save('photo.png')
+    save('photo.png');
 }
